ddd042b27192c3953f3a1257a46de50350822985
ollaidh <ollaidh@programmer.net>
2025-12-28 15:47:26 +0200

    moved bot check as separate function

--- main.go
+++ main.go
@@ -1,29 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"net/url"
-)
+var CONFIG Config
+
+func init() {
+	CONFIG = loadConfig()
+}
 
 func main() {
-	config, err := loadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-	url, err := url.JoinPath(
-		config.TelegramBot.Url,
-		"bot"+config.TelegramBot.Token,
-		"getMe")
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	body, err := io.ReadAll(response.Body)
-	bodyString := string(body)
-	fmt.Printf("Status: %s, Body: %s", response.Status, bodyString)
+	pingBot()
 
 }
---
+++ telegram_bot.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+func pingBot() {
+	url, err := url.JoinPath(
+		CONFIG.TelegramBot.Url,
+		"bot"+CONFIG.TelegramBot.Token,
+		"getMe")
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	pingError := ""
+
+	if response.Status != "200 OK" {
+		pingError = fmt.Sprintf("Response status = %s", response.Status)
+
+	}
+
+	body, err := io.ReadAll(response.Body)
+	var data map[string]interface{}
+
+	err = json.Unmarshal(body, &data)
+	if err == nil {
+		receivedId := strconv.Itoa(int(data["result"].(map[string]interface{})["id"].(float64)))
+		if receivedId != CONFIG.TelegramBot.Id {
+			pingError = fmt.Sprintf("Incorrect Id=%s received in response, expected %s", receivedId, CONFIG.TelegramBot.Id)
+		}
+
+	} else {
+		bodyString := string(body)
+		pingError = fmt.Sprintf("Failed to parse response body: %s Error: %s", bodyString, err)
+	}
+
+	if pingError != "" {
+		log.Fatalf("Bot Check failed: %s", pingError)
+	} else {
+		log.Printf("Bot Check Successful!")
+	}
+
+}
