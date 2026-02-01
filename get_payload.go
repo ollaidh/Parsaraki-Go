0f74f311c7bd760cea6ac83cb71065ad0387f3f7
ollaidh <ollaidh@programmer.net>
2026-02-01 14:00:55 +0200

    use pointer in interface

--- get_payload.go
+++ get_payload.go
@@ -8,7 +8,7 @@ type MessagePayloadGetter struct {
 }
 
 // send message from bot to chat
-func (pg MessagePayloadGetter) GetPayload(msg string, chatID int64) map[string]interface{} {
+func (pg *MessagePayloadGetter) GetPayload(msg string, chatID int64) map[string]interface{} {
 	payload := map[string]interface{}{
 		"chat_id": chatID,
 		"text":    msg,
@@ -21,7 +21,7 @@ type PhotoPayloadGetter struct {
 }
 
 // send picture from bot to chat
-func (pg PhotoPayloadGetter) GetPayload(photo string, chatID int64) map[string]interface{} {
+func (pg *PhotoPayloadGetter) GetPayload(photo string, chatID int64) map[string]interface{} {
 	payload := map[string]interface{}{
 		"chat_id": chatID,
 		"photo":   photo,
